c15b9d814d656fc08626a3ff53ecc88e1b5b9cb8
Brena <oliveirabrena365@gmail.com>
2023-12-19 16:16:34 -0300

    Adicionando atividade

--- andares.py
+++ andares.py
@@ -1,8 +1,10 @@
+## com for (ambos decrescente)
+
 for andar in range(20, 0, -1):
     if andar != 13:
       print(andar)
    
-## com While
+## com While (ambos decrescente)
 
 andar = 20
 while andar > 0:
