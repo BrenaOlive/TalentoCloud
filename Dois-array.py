239c36859a80e7ba0f0542e763ac796e2479e875
Brena <oliveirabrena365@gmail.com>
2024-01-11 13:13:32 -0300

    Adicionando Exercícios

---
+++ Dois-array.py
@@ -0,0 +1,7 @@
+# Array de produtos loja de cosméticos e nascimento de familiares
+produtos_loja_cosmetico = string["batom","blush","rimel"]
+anos_nascimento = [2001,1978,1996,1974]
+
+# Imprimindo os arrays
+print("\nProdutos da loja de coméstico:", produtos_loja_cosmetico)
+print("\nAnos de nascimento de familiares e amigos:", anos_nascimento)
